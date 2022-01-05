9a35d1416cbf95410b9d286f9b364250b5faaff9
RadhikaAgrawal <radhika10agrawal@gmail.com>
2022-01-05 12:51:57 +0530

    Updated

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express' )
 const app = express()
 const port = 3001
+var cors = require('cors')
+
+app.use(cors())
 
 app.get('/', (req, res) => {
   console.log(req);  
@@ -8,7 +11,7 @@ app.get('/', (req, res) => {
 })
 
 app.post('/', (req, res) => {
-    res.send('Post Hello World!')
+    res.send({returning:"success from Post"})
 })
   
 app.listen(port, () => {
